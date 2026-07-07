5a6ec8401319df71e1b71be54d7c0afef3f2fa27
Umair Ahsan <umairahsan899@gmail.com>
2026-07-08 00:20:07 +0500

    send email.

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,2 +1,8 @@
 import streamlit as st
 st.header("Contact Us")
+with st.form(key="email_forms"):
+    user_email = st.text_input("Your Email Address:")
+    message = st.text_area("Your Message:")
+    button=st.form_submit_button("Submit")
+    if button:
+        print("pressed")
---
+++ send_email.py
@@ -0,0 +1,11 @@
+import smtplib,ssl
+host = "smtp.gmail.com"
+port = 465
+username="umairahsan899@gmail.com"
+password="fjhvbrwdqxyjpkfl"
+receiver=""
+message=""
+context=ssl.create_default_context()
+with smtplib.SMTP_SSL(host,port,context=context) as server:
+    server.login(username,password)
+    server.sendmail(username,receiver)

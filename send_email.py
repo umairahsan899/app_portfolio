c49802ba362c01d7e1bee2d9181d30571befd1a8
Umair Ahsan <umairahsan899@gmail.com>
2026-07-08 11:44:29 +0500

    password protect completed....

--- send_email.py
+++ send_email.py
@@ -1,9 +1,10 @@
 import smtplib,ssl
+import os
 def send_email(receiver,message):
     host = "smtp.gmail.com"
     port = 465
     username = "umairahsan899@gmail.com"
-    password = "fjhvbrwdqxyjpkfl"
+    password = os.getenv("PASSWORD")
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(username, password)

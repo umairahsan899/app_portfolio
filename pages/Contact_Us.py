f00a70ea313f6df4fa5c121cb0c51fcee6c80d91
Umair Ahsan <umairahsan899@gmail.com>
2026-07-08 09:54:06 +0500

    send email using form.

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,8 +1,16 @@
 import streamlit as st
+import send_email
 st.header("Contact Us")
 with st.form(key="email_forms"):
     user_email = st.text_input("Your Email Address:")
     message = st.text_area("Your Message:")
+    message=f"""\
+    Subject: New email from {user_email}
+    
+    From: {user_email}
+    {message}
+    """
     button=st.form_submit_button("Submit")
     if button:
-        print("pressed")
+        send_email.send_email(user_email,message)
+        st.info("Email sent successfully")
--- send_email.py
+++ send_email.py
@@ -1,11 +1,15 @@
 import smtplib,ssl
-host = "smtp.gmail.com"
-port = 465
-username="umairahsan899@gmail.com"
-password="fjhvbrwdqxyjpkfl"
-receiver=""
-message=""
-context=ssl.create_default_context()
-with smtplib.SMTP_SSL(host,port,context=context) as server:
-    server.login(username,password)
-    server.sendmail(username,receiver)
+
+
+def send_email(receiver,message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "umairahsan899@gmail.com"
+    password = "fjhvbrwdqxyjpkfl"
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver,message)
+
+
+
